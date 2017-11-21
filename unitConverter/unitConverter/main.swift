004bbb8a57e98297074d8ba57824e433405c8dc5
Jack <somedd@naver.com>
2017-11-21 17:51:41 +0900

    Edit functions of unitconverter for legibility
    길이 단위 변환함수 완료. 하지만 전체적인 연산구조함수를 재구성해야됨

--- unitConverter/unitConverter/main.swift
+++ unitConverter/unitConverter/main.swift
@@ -4,8 +4,8 @@ import Swift
 func centiToMeter (centi : Double) -> Double {
     return centi * 0.01
 }
-func meterTocenti (m : Double) -> Int{
-    return Int(m * 100)
+func meterTocenti (m : Double) -> Double{
+    return m * 100
 }
 func centiToInch (centi : Double)-> Double{
     return centi / 2.54
@@ -67,49 +67,52 @@ func sliceString (str : String, unit : String) -> Double {
 //단위변환기
 //입력 : 입력받은 문자열, 바꾸려는 단위
 //출력 : 바뀐 숫자 + 바뀐 단위
-func lengthConvert (input : String, unit : String ) {
-    var temp = sliceString(str: input, unit: unit)
-    
-}
 func converter (input : String, output : String) {
-    if output.hasSuffix("cm") {
-        if input.hasSuffix("m") {
-            print(String(meterTocenti(m: sliceString(str: input, unit: "m"))) + output)
-        } else if input.hasSuffix("inch") {
-            print(String(inchToCenti(inch: sliceString(str: input, unit: "inch"))) + output)
-        } else if input.hasSuffix("yard") {
-            
+    var inputValue : Double = 0.0
+    var result : Double = 0.0
+    if output.hasSuffix(length[0]) {           //output : cm
+        if input.hasSuffix(length[1]) {        //m -> cm
+            inputValue = sliceString(str: input, unit: length[1])
+            result = meterTocenti(m: inputValue)
+        } else if input.hasSuffix(length[2]) { //inch -> cm
+            inputValue = sliceString(str: input, unit: length[2])
+            result = inchToCenti(inch: inputValue)
+        } else if input.hasSuffix(length[3]) { //yard -> cm
+            inputValue = sliceString(str: input, unit: length[3])
+            result = yardToCenti(yard: inputValue)
         }
-    } else if output.hasSuffix("m") {
-        if input.hasSuffix("cm") {
-            print(String(centiToMeter(centi: sliceString(str: input, unit: "cm"))) + output)
-        } else if input.hasSuffix("inch") {
-            print(String(centiToMeter(centi : inchToCenti(inch: sliceString(str: input, unit:"inch")))) + output)
+    } else if output.hasSuffix(length[1]) { //output : m
+        if input.hasSuffix(length[0]) { //cm -> m
+            inputValue = sliceString(str: input, unit: length[0])
+            result = centiToMeter(centi: inputValue)
+        } else if input.hasSuffix(length[2]) { //inch -> centi -> m
+            inputValue = sliceString(str: input, unit: length[2])
+            result = centiToMeter(centi: inchToCenti(inch: inputValue))
+        } else if input.hasSuffix(length[3]) { //yard -> cm -> m
+            inputValue = sliceString(str: input, unit: length[3])
+            result = centiToMeter(centi: yardToCenti(yard: inputValue))
         }
-    } else if output.hasSuffix("inch") {
-        if input.hasSuffix("cm") {
-            print(String(centiToInch(centi: sliceString(str: input, unit: "cm"))) + output)
-        } else if input.hasSuffix("m") {
-            print(String(centiToInch(centi: Double(meterTocenti(m: sliceString(str: input, unit: "m"))))) + output)
+    } else if output.hasSuffix(length[2]) {
+        if input.hasSuffix(length[0]) {
+            inputValue = sliceString(str: input, unit: length[0])
+            result = centiToInch(centi: inputValue)
+        } else if input.hasSuffix(length[1]) {
+            inputValue = sliceString(str: input, unit: length[1])
+            result = centiToInch(centi: meterTocenti(m: inputValue))
+        } else if input.hasSuffix(length[3]) { //yard -> centi -> inch
+            inputValue = sliceString(str: input, unit: length[3])
+            result = centiToInch(centi: yardToCenti(yard: inputValue))
         }
     }
+    print(String(result) + output)
 }
 var length : Array = ["cm", "m", "inch", "yard"]
 var weight : Array = ["kg","g","oz","lb"]
 var volume : Array = ["l","pt","qt","gal"]
-func unitCheck (input : String, output : String) {
-    switch output {
+func unitCheck (value : Double, inputString : String, outputString : String) {
+    switch outputString {
     case length[0]...length[3]: //length
-        if output == length[0] { //output : cm
-           //m or inch or yard -> cm
-            //input : 입력받은 값, length
-        }  else if output == length[1] { //output : m
-           
-        }  else if output == length[2] { //output : inch
-            
-        }  else if output == length[3] { //output : yard
-            
-        }
+        converter(input: inputString, output: outputString)
     case weight[0]...weight[3]:
         return
     case volume[0]...volume[3]:
@@ -118,8 +121,14 @@ func unitCheck (input : String, output : String) {
         print("지원하지 않는 단위입니다.")
     }
 }
+func unitChange (value : Double, inputUnit : String, outputUnit : String) {
+    
+}
 var str  = readLine()
-let arrOfstr = str!.components(separatedBy: " ")
-unitCheck(input : arrOfstr[0], output: arrOfstr[1])
-converter(input: arrOfstr[0], output: arrOfstr[1])
+var arrOfstr = str!.components(separatedBy: " ")
+var inputu = arrOfstr[0]
+print(inputu)
+
+//unitCheck(inputString : arrOfstr[0], outputString : arrOfstr[1])
+//converter(input: arrOfstr[0], output: arrOfstr[1])
 
