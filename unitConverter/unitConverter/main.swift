06e7c31bd79878e19ff4e6cd09b2e7284d4bcdb7
Jack <somedd@naver.com>
2017-11-24 15:11:20 +0900

    Complete basic function of converter
    기본구현 완료, 출력값 포맷팅과 예외처리 구현예정

--- unitConverter/unitConverter/main.swift
+++ unitConverter/unitConverter/main.swift
@@ -14,50 +14,70 @@ struct units {
     static let error : [String : Double] = ["error" : 0]
 }
 //입력받은 문자열을 배열로 나누기
-func inputstrToArr (string : String) -> Array<String> {
+func inputstrToArr (_ string : String) -> Array<String> {
     let arrOfStr = string.components(separatedBy: " ")
     return arrOfStr
 }
+//계산하는 함수
+func converter (_ inputnum : Double, _ inputunit : (String?,[String:Double]), _ outputunit :(String?,[String:Double]) ,_ isBase : Bool) -> Double {
+    var result : Double = 0.0
+    if isBase == true {
+        result = inputnum / (outputunit.1)[outputunit.0!]!
+    } else {
+        result =  inputnum * (inputunit.1)[inputunit.0!]! / (outputunit.1)[outputunit.0!]!
+    }
+    return result
+}
 //단위를 찾는 함수
-func searchUnit (inputString : String) -> (key : String?, unitDic : String){
+func searchUnit (_ inputString : String) -> (key : String?, unitDic : [String:Double]){
     for key in units.lengthDic.keys {
-        if inputString.hasSuffix(key) {return (key, "lengthDic")}
+        if inputString.hasSuffix(key) {return (key, units.lengthDic)}
     }
     for key in units.weightDic.keys {
-        if inputString.hasSuffix(key) {return (key, "weightDic")}
+        if inputString.hasSuffix(key) {return (key, units.weightDic)}
     }
     for key in units.volumeDic.keys {
-        if inputString.hasSuffix(key) {return (key, "volumeDic")}
+        if inputString.hasSuffix(key) {return (key, units.volumeDic)}
     }
-    return (nil, "")
+    return (nil, units.error)
 }
-//func checkBase (_ key : String,_ unitDic : String) -> (key : String, unitDic : String){
-//    switch unitDic {
-//    case "lengthDic": return key
-//    case "weightDic": return key
-//    case "volumeDic": return key
-//    default : return
-//    }
-//}
-//입력받은 문자열에서 계산하려는 숫자만 분리
-func sliceString (str : String, unit : String) -> Double {
+//기본단위인지 확인하는 함수
+func checkBase (_ unit : String) -> Bool {
+    switch unit {
+    case Baseunits.cm.rawValue:
+        return true
+    case Baseunits.g.rawValue :
+        return true
+    case Baseunits.l.rawValue :
+        return true
+    default:
+        return false
+    }
+}
+//숫자를 빼는 함수
+func sliceString (_ str : String, _ unit : String) -> Double {
     let numbers =  str.components(separatedBy: "\(unit)")
     return Double(numbers[0])!
 }
-func unitConverter (inputString : String) {
-    let strArr = inputstrToArr(string: inputString)
+func unitConverter (_ inputString : String) {
+    let strArr = inputstrToArr(inputString)
     switch strArr.count {
     case 0:
         return
     case 1:
         return
     case 2:
-        let tempUnit = searchUnit(inputString: strArr[0])
-        checkBase(tempUnit.key, tempUnit.unitDic)
+        let inputUnit = searchUnit(strArr[0])
+        let outputUnit = searchUnit(strArr[1])
+        let inputNum = sliceString(strArr[0], inputUnit.key!)
+        let chkBase = checkBase(inputUnit.key!)
+        let resultNum = converter(inputNum, inputUnit, outputUnit, chkBase)
+        print("\(resultNum)" + outputUnit.key!)
+        return
     default:
         print("지원하지 않는 단위입니다.")
     }
 }
 var str = readLine()
-unitConverter(inputString: str!)
+unitConverter(str!)
 
